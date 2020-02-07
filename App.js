0c3f5ee3594fa7f4e35ca20aa1e5e0df33e33957
Iqra Nathani <iqranathani>
2020-02-07 21:14:19 +0500

    created addblogs and deleteblogs

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
+import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
 
 const navigator = createStackNavigator({
-  Index:IndexScreen
+  Index:IndexScreen,
+  Show:ShowScreen
 },{
   initialRouteName: 'Index',
   defaultNavigationOptions: {
@@ -16,6 +19,8 @@ const navigator = createStackNavigator({
 const App = createAppContainer(navigator);
 
 export default () => {
-  return <App />;
+  return <Provider>
+            <App />
+         </Provider>
 };
 
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button,TouchableOpacity } from 'react-native';
+import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
+    const { state, addBlogPost, deleteBlogPost} = useContext(Context);
+
     return (
         <View>
-            <Text>IndexScreen</Text>
+            <Button
+                title="Add Post"
+                onPress={addBlogPost}
+            />
+            <FlatList
+                data={state}
+                keyExtractor={(blogPosts) => blogPosts.title}
+                renderItem={({ item }) => {
+                    return <View style={styles.row} >
+                        <Text style={styles.title} >{item.title} - {item.id}</Text>
+                        <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                            <Feather style={styles.iconStyle} name="trash"/>
+                        </TouchableOpacity>
+                        
+                    </View>
+                }}
+            />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20 ,
+        paddingHorizontal: 10 ,
+        borderTopWidth: 1 ,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 22
+    }
+});
 
 export default IndexScreen;
